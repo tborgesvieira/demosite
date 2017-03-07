74f0bd4d7713ac3867f1e6181326ea1d3d5fede7
Thiago Borges Vieira <thiago@upgradesolutions.com.br>
2017-03-07 00:19:55 -0300

    site

---
+++ Site/AspNetMvc4Vs15/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMvc4Vs15.Global" Language="C#" %>
---
+++ Site/AspNetMvc4Vs15/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AspNetMvc4Vs15
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
